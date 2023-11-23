0237f7956b5f4d1e238d380cb575f0c9c9813a29
T4824 <T4824@LTILMAA21202504.lntinfotech.com>
2023-11-23 20:22:55 +0530

    Changed Encap

--- src/java_class/Encap.java
+++ src/java_class/Encap.java
@@ -1,7 +1,18 @@
 package java_class;
 
 public class Encap {
-
+//Encapsulation:
+	private int x;
+	
+	//getter for get a value
+	public int getter(){
+		return x;
+	}
+	//setter for access a value
+	public void setter(int a) {
+		x = a;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
